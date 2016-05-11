478827385e1c115d132dd025f8aa2b073d596cb9
Cesar Tardaguila <cesar@yyresearch.com>
2016-05-11 16:51:35 +0800

    Initial commit

---
+++ UI/UIClass.h
@@ -0,0 +1,13 @@
+//
+//  UIClass.h
+//  UI
+//
+//  Created by Cesar Tardaguila on 5/11/16.
+//  Copyright © 2016 Cesar Tardaguila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIClass : NSObject
+
+@end
---
+++ UI/UIClass.m
@@ -0,0 +1,13 @@
+//
+//  UIClass.m
+//  UI
+//
+//  Created by Cesar Tardaguila on 5/11/16.
+//  Copyright © 2016 Cesar Tardaguila. All rights reserved.
+//
+
+#import "UIClass.h"
+
+@implementation UIClass
+
+@end
